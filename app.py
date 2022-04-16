f30a70175108db5941ef45171bf1f361ac08a153
Moopz-Dev <pongzthor@gmail.com>
2022-04-16 18:56:37 +0700

    feat: implements database and ORM ; includes example transactions

--- app.py
+++ app.py
@@ -1,6 +1,22 @@
 from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mystatement.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+
+class Statement(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.String(50), nullable=False)
+    name = db.Column(db.String(100), nullable=False)
+    amount = db.Column(db.Integer, nullable=False)
+    category = db.Column(db.String(50), nullable=False)
+
+
+db.create_all()
 
 
 @app.route("/")
@@ -14,8 +30,10 @@ def addStatement():
     name = request.form["title"]
     amount = request.form["amount"]
     category = request.form["category"]
-    print("Date =", date, "Title =", name,
-          "Amount =", amount, "Category =", category)
+    statement = Statement(date=date, name=name,
+                          amount=amount, category=category)
+    db.session.add(statement)
+    db.session.commit()
     return redirect("/")
 
 
