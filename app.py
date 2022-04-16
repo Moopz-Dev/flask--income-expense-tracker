801eea85b563c54b935da02d67bd8520595ad4db
Moopz-Dev <pongzthor@gmail.com>
2022-04-16 18:34:15 +0700

    feat: implements form processing and redirect

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
@@ -6,6 +6,17 @@ app = Flask(__name__)
 @app.route("/")
 def addForm():
     return render_template("addForm.html")
+
+
+@app.route("/addStatement", methods=["POST"])
+def addStatement():
+    date = request.form["date"]
+    name = request.form["title"]
+    amount = request.form["amount"]
+    category = request.form["category"]
+    print("Date =", date, "Title =", name,
+          "Amount =", amount, "Category =", category)
+    return redirect("/")
 
 
 if __name__ == "__main__":
